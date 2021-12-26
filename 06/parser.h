f1b753aba171a53ba8a0419fbc81a4ba3d37dfd0
Michael Schenck <mps0@protonmail.com>
2021-12-27 00:12:24 +0100

    parser

---
+++ 06/parser.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define LINE_MAX_BYTES 512
+
+extern FILE* fp;
+
+int peek();
+
+bool hasMoreLines();
+
+void advance();
+
+void process(char* line, int* n);
+
+void parse();
