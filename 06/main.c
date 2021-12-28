a29bcab10a1ee0936f8e2970f419c5db788530a9
Michael Schenck <mps0@protonmail.com>
2021-12-28 21:09:40 +0100

    working

--- 06/main.c
+++ 06/main.c
@@ -4,7 +4,8 @@
 
 #include "parser.h"
 
-FILE* fp;
+FILE* fREAD;
+FILE* fWRITE;
 
 int main(int argc, char **argv)
 {
@@ -21,36 +22,52 @@ int main(int argc, char **argv)
 
     char* pc = argv[1];
     bool extensionFound = false;
+    int extensionLoc = 0;
+    int i = 0;
     while(*pc != '\0')
     {
         if(*pc == '.')
         {
             extensionFound = true;
+            extensionLoc = i;
 
             if(strcmp(pc + 1, "asm") != 0)
             {
-                printf("Incaptible file extension: .%s\n", pc + 1);
+                printf("Incompatible file extension: .%s\n", pc + 1);
                 return -1;
             }
         }
         ++pc;
+        ++i;
     }
 
     if(!extensionFound)
     {
-        printf("No file extension found\n");
+        printf(".asm file extension not found\n");
         return -1;
     }
 
-     fp = fopen(argv[1], "r");
-    if(!fp)
+     fREAD = fopen(argv[1], "r");
+    if(!fREAD)
     {
         printf("Could not open file %s\n", argv[1]);
         return -1;
     }
+    char name[extensionLoc + 2];
+    memcpy(name, argv[1], extensionLoc) ;
+
+     fWRITE= fopen(strcat(name, ".hack"), "w");
+    if(!fWRITE)
+    {
+        printf("Could not create output machine code file!\n");
+        return -1;
+    }
+
+
     parse();
 
-    fclose(fp);
+    fclose(fREAD);
+    fclose(fWRITE);
 
     return 0;
 }
--- 06/parser.c
+++ 06/parser.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #include "parser.h"
 
 #define LINE_MAX_BYTES 512
+#define WORD_BIT_SIZE 16
 
-extern FILE* fp;
+extern FILE* fREAD;
+extern FILE* fWRITE;
+
+typedef enum {
+    A_INSTRUCTION,
+    C_INSTRUCTION,
+    L_INSTRUCTION
+} INSTRUCTION_TYPE;
 
 int peek()
 {
-    char c = getc(fp);
-    fseek(fp, -1, SEEK_CUR);
+    char c = getc(fREAD);
+    fseek(fREAD, -1, SEEK_CUR);
 
     return c;
 }
@@ -30,10 +39,10 @@ void advance()
 {
     bool goToNextLine = false;
 
-    char c = getc(fp);
+    char c = getc(fREAD);
     while(c == ' ')
     {
-        c = getc(fp);
+        c = getc(fREAD);
     }
 
     if (c == '/' && peek()  == '/')
@@ -45,25 +54,25 @@ void advance()
     {
         while(c != '\n')
         {
-            c = getc(fp);
+            c = getc(fREAD);
         }
         advance();
         return;
     }
-    fseek(fp, -1, SEEK_CUR);
+    fseek(fREAD, -1, SEEK_CUR);
 }
 
 void process(char* line, int* n)
 {
     *n = 0;
     int startOfSpace;
-    char c = getc(fp);
+    char c = getc(fREAD);
     while(c != '\n' && c != EOF && *n < LINE_MAX_BYTES - 2)
     {
         if (c == '/' && peek()  == '/')
         {
             *n = startOfSpace;
-            fseek(fp, -1, SEEK_CUR);
+            fseek(fREAD, -1, SEEK_CUR);
             advance();
             break;
         }
@@ -74,14 +83,229 @@ void process(char* line, int* n)
 
         line[*n] = c;
         ++(*n);
-        c = getc(fp);
+        c = getc(fREAD);
     }
     line[*n] = '\0';
 }
 
-void parse()
+// positive only
+int decimalStringToInt(char* s)
 {
 
+    int length = strlen(s);
+
+    int sum = 0;
+    int mult = 1;
+    bool negative = false;
+    int end = 0;
+    if(s[0] == '-') {
+        negative = true;
+        ++end;
+    }
+    int i = length - 1;
+    while(i >= end)
+    {
+        sum += mult * (s[i] - 48);
+        mult *= 10;
+        --i;
+    }
+
+    if(negative) {
+        return -sum;
+    }
+    else {
+        return sum;
+    }
+}
+
+void decimalToBinary(int n, char* binary)
+{
+    if(n < 0){
+        n += 32768; // 2^15
+        binary[15] = '1';
+    }
+    else {
+        binary[15] = '0';
+    }
+    for(int i = 0; i < WORD_BIT_SIZE - 1; ++i)
+    {
+        //binary[15 - i] = n % 2 + 48;
+        binary[i] = n % 2 + 48;
+        n /= 2;
+    }
+    binary = strrev(binary);
+}
+
+INSTRUCTION_TYPE instructionType(char firstByte)
+{
+    switch(firstByte)
+    {
+        case '@' :
+            printf("@\n");
+            return A_INSTRUCTION;
+
+        case '(' :
+            printf("(\n");
+            return L_INSTRUCTION;
+
+        default :
+            printf("DEFAULT\n");
+            return C_INSTRUCTION;
+    }
+}
+
+
+typedef struct {
+    char dest[4];
+    char bits[4];
+} DMap;
+
+typedef struct {
+    char comp[7];
+    char bits[7];
+} CMap;
+
+typedef struct {
+    char jump[4];
+    char bits[4];
+} JMap;
+
+
+
+DMap dMap[7] =
+{
+    {"M", "001"},
+    {"D", "010"},
+    {"DM", "011"},
+    {"A", "100"},
+    {"AM", "101"},
+    {"AD", "110"},
+    {"ADM", "111"}
+};
+
+CMap cMap[18] =
+{
+    {"0", "101010"},
+    {"1", "111111"},
+    {"-1", "111010"},
+    {"D", "001100"},
+    {"X", "110000"},
+    {"!D", "001101"},
+    {"!X", "110001"},
+    {"-D", "001111"},
+    {"-X", "110011"},
+    {"D+1", "011111"},
+    {"X+1", "110111"},
+    {"D-1", "001110"},
+    {"X-1", "110010"},
+    {"D+X", "000010"},
+    {"D-X", "010011"},
+    {"X-D", "000111"},
+    {"D&A", "000000"},
+    {"D|A", "010101"},
+};
+
+JMap jMap[7] =
+{
+    {"JGT", "001"},
+    {"JEQ", "010"},
+    {"JGE", "011"},
+    {"JLT", "100"},
+    {"JNE", "101"},
+    {"JLE", "110"},
+    {"JMP", "111"},
+};
+
+char* dest(char* line)
+{
+    int eqLoc = 0;
+    while(line[eqLoc] != '=')
+    {
+        eqLoc++;
+    }
+    printf("EQLOC: %d\n", eqLoc);
+    char* rval = calloc(eqLoc + 1, sizeof(char));
+    memcpy(rval, line, eqLoc);
+    rval[eqLoc] = '\0';
+    return rval;
+}
+
+
+char* comp(char* line)
+{
+    int eqLoc = 0;
+    while(line[eqLoc] != '=')
+    {
+        //printf("line[eqLoc] = %c\n", line[eqLoc]);
+        //dest[eqLoc] = line[eqLoc];
+        eqLoc++;
+    }
+
+    return line + eqLoc + 1;
+}
+
+char* destBin(char* d) {
+
+    char* rval = calloc(4, sizeof(char));
+    // get ddd
+    for(int i = 0; i < 7; i++) {
+        if(strcmp(d, dMap[i].dest) == 0)
+        {
+           memcpy(rval, dMap[i].bits, 4);
+           break;
+        }
+    }
+    return rval;
+}
+
+char* compBin(char* c)
+{
+
+    char* rval = calloc(7, sizeof(char));
+
+    // get cccccc
+    for(int i = 0; i < 18; i++) {
+        if(strcmp(c, cMap[i].comp) == 0)
+        {
+           memcpy(rval, cMap[i].bits, 7);
+           break;
+        }
+    }
+           return rval;
+}
+
+
+
+void parseCinstruction(char* line, char* bin)
+{
+
+    //remember to clean
+    char* d = dest(line);
+    printf("dest return: %s\n", d);
+    char* ddd = destBin(d);
+    printf("ddd: %s\n", ddd);
+
+    // get cccccc
+    //char* RHS = line + eqLoc + 1;
+    char* c = comp(line);
+    printf("comp Returns: %s\n", c);
+    char* cccccc = compBin(c);
+    printf("cccccc: %s\n", ddd);
+
+
+    //TODO
+    char jjj[4] = "000";
+    // get jjj
+    //
+    char* res = strcat(cccccc, ddd);
+    res = strcat(res, jjj);
+    memcpy(bin + 4, res, 15);
+    free(ddd);
+    free(cccccc);
+}
+
+void parse()
+{
     char line[LINE_MAX_BYTES];
     int length;
     while(hasMoreLines())
@@ -91,6 +315,42 @@ void parse()
         if(length == 0) {
             continue;
         }
-        printf("extracted line: %s\n", line);
+
+        char bin[WORD_BIT_SIZE + 1];
+        bin[0] = '1';
+        bin[0] = '1';
+        bin[0] = '1';
+        bin[16] = '\0';
+        printf("line: %s\n", line);
+        switch(instructionType(line[0])) {
+            case A_INSTRUCTION:
+                {
+
+                    int dec = decimalStringToInt(line + 1);
+                    //printf("decString: %d", decString);
+                    decimalToBinary(dec, bin);
+                    //printf("converted line: %s\n", bin);
+                    break;
+                }
+
+            case C_INSTRUCTION:
+                {
+                    printf("PARSING C INSTRUCTION\n");
+                    parseCinstruction(line, bin);
+            printf("bin: %s\n", bin);
+                    break;
+                }
+
+            case L_INSTRUCTION:
+                {
+
+                    break;
+                }
+        }
+
+        fputs(bin, fWRITE);
+        fputc('\n', fWRITE);
+
+        //printf("extracted line: %s\n", line);
     }
 }
